d7bfc997e67be476cd58fc4b605f0bd0682162ee
Neah Nieto <nrpcnieto@gmail.com>
2020-01-08 11:53:20 -0600

    Add routes for CRUD actions for edible.routes.js

---
+++ app/routes/edible.routes.js
@@ -0,0 +1,18 @@
+module.exports = function(app) {
+  var edibles = require("../controllers/edible.controller.js");
+
+  // Edible index
+  app.get("/api/edibles", edibles.index);
+
+  // Edible create
+  app.post("/api/edibles", edibles.create);
+
+  // Edible show
+  app.get("/api/edibles/:id", edibles.show);
+
+  // Edible update
+  app.put("/api/edibles/:id", edibles.update);
+
+  // Edible destroy
+  app.delete("/api/edibles/:id", edibles.destroy);
+};
