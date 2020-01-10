5f956e57c731f955955da9c805cc405c102ee9aa
Neah Nieto <nrpcnieto@gmail.com>
2020-01-10 11:25:25 -0600

    Connect everything back to entry point (server.js)

--- server.js
+++ server.js
@@ -1,17 +1,24 @@
-const express = require("express");
+import config from "dotenv";
+import express from "express";
 const app = express();
-const port = process.env.PORT || 3000;
 
-//Parse requests data
-const bodyParser = require("body-parser");
+import bodyParser from "body-parser";
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Setup a basic get request method
-app.get("", (req, res) =>
+const port = process.env.PORT || 8081;
+
+app.use("/api/v1/edibles", edibleRoutes);
+
+// when a random route is inputed
+app.get("*", (req, res) =>
   res.status(200).send({
-    message: "Hello world!"
+    message: "Welcome to this API."
   })
 );
 
-app.listen(port);
+app.listen(port, () => {
+  console.log(`Server is running on PORT ${port}`);
+});
+
+export default app;
