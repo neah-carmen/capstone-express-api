12429cb95561f488015bdac16ea440f2c2666310
Neah Nieto <nrpcnieto@gmail.com>
2020-01-13 21:05:25 -0600

    Add validations to User model

--- api/server/src/models/user.js
+++ api/server/src/models/user.js
@@ -3,9 +3,20 @@ module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define(
     "User",
     {
-      email: DataTypes.STRING,
-      username: DataTypes.STRING,
-      password: DataTypes.STRING
+      email: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false
+      },
+      username: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false
+      }
     },
     {}
   );
